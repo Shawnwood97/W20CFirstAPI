83b82140980671cb4858383c5652d972263005c5
Shawn Wood <Shawnwood97@gmail.com>
2021-06-10 20:26:27 -0600

    initial push

---
+++ db.py
@@ -0,0 +1,64 @@
+import mariadb
+import dbcreds
+import traceback
+
+
+def openConnection():
+  try:
+    return mariadb.connect(
+        user=dbcreds.user,
+        password=dbcreds.password,
+        host=dbcreds.host,
+        port=dbcreds.port,
+        database=dbcreds.database,
+    )
+
+  except:
+    print("Error opening connextion to DB!")
+    traceback.print_exc()
+    return None
+
+
+def closeConnection(conn):
+  if(conn == None):
+    return True
+  try:
+    conn.close()
+    return True
+
+  except:
+    print("Error closing connection to DB!")
+    traceback.print_exc()
+    return False
+
+
+def openCursor(conn):
+  # ! Not sure I need this here since the except block will close it!?
+  # if(conn == None):
+  #   print('No connection to database, closing your connection!')
+  #   return None
+  try:
+    return conn.cursor()
+  except:
+    print("Error opening cursor on DB, closing connection!")
+    traceback.print_exc()
+    return None
+
+
+def closeCursor(cursor):
+  if(cursor == None):
+    return True
+  try:
+    cursor.close()
+    return True
+
+  except:
+    print("Error closing cursor on DB!")
+    traceback.print_exc()
+    return False
+
+
+def closeAll(conn, cursor):
+  closeCursor(cursor)
+  closeConnection(conn)
+  print('Cursor and connection closed!')
